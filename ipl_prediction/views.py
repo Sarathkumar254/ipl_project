200172f45ab080af35f33c8edc72775980746b95
Sarathkumar Pillagovula <49829889+Sarathkumar254@users.noreply.github.com>
2021-12-01 08:00:35 +0530

    Add files via upload

---
+++ ipl_prediction/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from .models import IPL
+import numpy as np
+import pickle
+import sklearn
+
+# Create your views here.
+# Load the Random Forest CLassifier model
+filename = 'ipl_prediction/first-innings-score-lr-model.pkl'
+regressor = pickle.load(open(filename, 'rb'))
+
+
+def home(request):
+    return render(request, 'ipl_prediction/index.html')
+
+
+def predict(request):
+    temp_array = list()
+
+    if request.method == 'POST':
+
+        batting_team = request.POST['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
+
+        bowling_team = request.POST['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
+
+        overs = float(request.POST['overs'])
+        runs = int(request.POST['runs'])
+        wickets = int(request.POST['wickets'])
+        runs_in_prev_5 = int(request.POST['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.POST['wickets_in_prev_5'])
+
+        temp_array = temp_array + [runs, wickets, overs, runs_in_prev_5, wickets_in_prev_5]
+
+        data = np.array([temp_array])
+        my_prediction = abs(int(regressor.predict(data)[0]))
+        data = {"lower_limit":my_prediction - 5, "upper_limit":my_prediction + 5}
+        return render(request, 'ipl_prediction/result.html', {'result': data})
+    elif request.method == 'GET':
+    	return render(request, 'ipl_prediction/index.html')
